70703e32cfeaea595f0f45460688378fa80f02d2
Dawid Wilk <38471575+DawidWilk81@users.noreply.github.com>
2021-07-22 10:47:59 +0200

    Update settings.py

--- Biblioteka/Biblioteka/settings.py
+++ Biblioteka/Biblioteka/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -150,3 +150,5 @@ REST_FRAMEWORK = {
     ),
     'NON_FIELD_ERRORS_KEY': 'global',
 }
+
+django_heroku.settings(locals())
